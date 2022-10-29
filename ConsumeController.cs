13482172acf10ba8cc4a4f2d5f05a477148dc2f3
greattgruji <104081859+greattgruji@users.noreply.github.com>
2022-10-29 17:56:04 +0530

    Add files via upload

---
+++ ConsumeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebMyAppAspdotnetmvcapi.Models;
+
+namespace WebMyAppAspdotnetmvcapi.Controllers
+{
+    public class ConsumeController : Controller
+    {
+        // GET: Consume
+        HttpClient client = new HttpClient(); 
+        public ActionResult Index()
+        {
+            List<Student> list = new List<Student>();
+            client.BaseAddress = new Uri("http://localhost:52768/api/NewApi");
+            var response = client.GetAsync("NewApi");
+            response.Wait();
+            var test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Student>>();
+                display.Wait();
+                list = display.Result;
+            }
+            return View(list);
+        }
+    }
+}
---
+++ NewApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebMyAppAspdotnetmvcapi.Models;
+
+namespace WebMyAppAspdotnetmvcapi.Controllers
+{
+    public class NewApiController : ApiController
+    {
+        GoogleEntities db = new GoogleEntities();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<Student> obj = db.Students.ToList();
+            return Ok(obj);
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index(int id)
+        {
+            var obj = db.Students.Where(m => m.Id == id).FirstOrDefault();
+            return Ok(obj);
+        }
+    }
+}
